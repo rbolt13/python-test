aaa198d38bbd3b1cdd96402841f7b14c3f4d2d82
rbolt13 <rbolt@pdx.edu>
2021-02-24 19:29:09 -0800

    added a few comments

--- test.py
+++ test.py
@@ -67,26 +67,27 @@ def deftype():
     print(type(x),type(y),type(z))
     print("\n")
 
+#int , float, complex
 x = 1
 y = 2.8
 z = 1j
 deftype()
-
+#int
 x = 1
 y = 35656222554887711
 z = -3255522
 deftype()
-
+#float
 x = 1.10
 y = 1.0
 z = -35.59
 deftype()
-
+#float
 x = 35e3
 y = 12E4
 z = -87.7e100
 deftype()
-
+#complex
 x = 3+5j
 y = 5j
 z = -5j
