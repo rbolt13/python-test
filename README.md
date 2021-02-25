7d311ed0b0ae4041b8e237d753d5aa7efc9fd39a
rbolt13 <rbolt@pdx.edu>
2021-02-24 18:29:02 -0800

    updated python code, and credited W3

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Learning the basics of Python and Git.
 2021
+
+Feb. 2021 working on the basics of python with w3schools online. 
--- test.py
+++ test.py
@@ -1,4 +1,35 @@
-a = 5
+a = "Hello User! \nThis program goes over some basic python."
+b = "Let's begin\n"
 print(a)
-b = "I love Tanner!"
 print(b)
+
+print("Is 5 > 2?")
+
+if 5 > 2:
+    print("Five is greater than two!\n")
+
+#casting
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x,y,z)
+print(type(x),type(y),type(z))
+
+j = "\ndouble quotes are the same"
+k = 'as sinlge quotes'
+
+print(j)
+print(k)
+
+A = "\nVariable names are case-sensitive.\n"
+print(A)
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x = y = z = "Oranges"
+print(x)
+print(y)
+print(z)
