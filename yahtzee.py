d96d7feb9ab744ec55fa9f10c63807ecd37dd400
rbolt13 <rbolt@pdx.edu>
2021-03-10 20:57:27 -0800

    played around some more with a new code I cound on a google share drige

--- yahtzee.py
+++ yahtzee.py
@@ -1,25 +1,45 @@
 """
 This is will eventually be the game of yahtzee.
-For now, it outputs and saves the roll of six dice. 
+For now, it outputs and saves the roll of six dice.
 -----------------------------------------------
 Sources :
 
 Write Yahtzee in Python :
-https://betterprogramming.pub/interview-questions-write-yahtzee-in-python-72695550d84e
-"""
+https://colab.research.google.com/drive/1I_0h72NlXz7YH29FES0qNyus_NvlsPV2
+https://stackoverflow.com/questions/44008489/dice-rolling-simulator-in-python/44009898
 
+"""
+"""
 import random
 
 print("Welcome to Yahtzee")
 
-a = random.randrange(1,6)
-b = random.randrange(1,6)
-c = random.randrange(1,6)
-d = random.randrange(1,6)
-e = random.randrange(1,6)
-txt = "You rolled : {} , {} , {} , {} , {}"
-print(txt.format(a, b, c, d, e))
+txt = "Your first roll is {}, {}, {}, {}, {}, {}"
 
-dice_list = (a, b, c, d, e)
-print(type(dice_list))
-print(dice_list)
+die1 = random.randint(1,6)
+die2 = random.randint(1,6)
+die3 = random.randint(1,6)
+die4 = random.randint(1,6)
+die5 = random.randint(1,6)
+die6 = random.randint(1,6)
+
+print(txt.format(die1, die2, die3, die4, die4, die5, die6))
+
+"""
+import random
+random.randint(1,6)
+def rollDice(n):
+  rolls = [random.randint(1,6) for i in range (n)]
+  return rolls
+
+currRolls = rollDice(5)
+print(currRolls)
+
+counts = [currRolls.count(1), currRolls.count(2),currRolls.count(3), currRolls.count(4), currRolls.count(5), currRolls.count(6)]
+print(counts)
+
+maxCount = max(counts)
+print (maxCount)
+
+maxVal = counts.index(maxCount)
+print(maxVal+1)
